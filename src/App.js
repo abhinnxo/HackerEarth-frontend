3fe9531723e6171b79b2ebb7aacd68ad2d1be1bd
Abhinn Krishn <abhinnk21@gmail.com>
2021-10-30 22:54:26 +0530

    Added Dashboard Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import CenterBoard from "./components/Dashboard";
+
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <Route exact path="/dashboard">
+        <Navbar />
+        <CenterBoard />
+      </Route>
+    </BrowserRouter>
   );
 }
 
